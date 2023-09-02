06986febe418ac2cb26e9817a39cf3726420de2f
sathish1211 <sathish2001p@gmail.com>
2023-09-02 09:30:23 +0530

    filter

--- javascript/filter/age greater than given age.js
+++ javascript/filter/age greater than given age.js
@@ -1,1 +1,3 @@
-a=[{name:Dinesh,age:45},{name:Jeeva,age:18},{name:sarath,age:25},{name:Raja,age:23},]
+a=[{name:"Dinesh",age:45},{name:"Jeeva",age:18},{name:"sarath",age:25},{name:"Raja",age:23}]
+b=a.filter(e=>e.age>23)
+console.log(b)
