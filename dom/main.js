3a9ee151cc0354ea8e54ad9fb67e8056ae3da6ae
sathish1211 <sathish2001p@gmail.com>
2023-09-25 11:13:51 +0530

    dom

---
+++ dom/main.js
@@ -0,0 +1,13 @@
+// const person={
+//     name:"sathish",
+//     place:"namakkal",
+//     greetUser(){
+//         return `hello, myname is ${this.name} and i am from ${this.place}`;
+//     }
+// };
+// const nameinput=prompt("what ois yor name");
+// const placeinput=prompt("wher you from");
+
+// person.name=nameinput;
+// person.place=placeinput;
+// document.write(person.greetUser());
