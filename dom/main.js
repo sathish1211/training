671a94443e533263ac8fde223bd4dbc995aeedb4
sathish1211 <sathish2001p@gmail.com>
2023-09-30 12:58:56 +0530

    dom

--- dom/main.js
+++ dom/main.js
@@ -1,13 +1,21 @@
 const event=document.getElementById("event")
 const todolist=document.getElementById("todoList")
 event.addEventListener('keypress',(e)=>{
-   if(e.code === "Enter"){
+   if(e.code === "Enter" && e.target.value !=="")
+   {
     let todo=  e.target.value;
     let todoItem=document.createElement('li');
     let todoId=new Date().getTime();
     todoItem.innerHTML=`             
      <input id="${todoId}" type="checkbox">
      <label for="${todoId}"  >${todo}</label> ` ;
-    todolist.appendChild(todoItem)
+    todolist.appendChild(todoItem);
+    event.value="";
    }
 });
+todolist.addEventListener('click',(e)=>{
+   if(e.target.checked)
+      e.target.parentElement.style.textDecoration="line-through";
+   else
+   e.target.parentElement.style.textDecoration="none";
+})
