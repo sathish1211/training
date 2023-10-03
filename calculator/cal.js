b1c8ce5d3b39e50d17385a30248aea00180fc886
sathish1211 <sathish2001p@gmail.com>
2023-10-03 14:34:55 +0530

    myself

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,10 +1,62 @@
 var textbox = document.getElementById("textbox");
 var buttons = document.getElementsByTagName("button");
-var display=document.getElementsByClassName("display").innerText="sathish"
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-        textbox.value += this.innerText;
-        display.textContent+=this.innerText;
-    });
-}
+var result = document.getElementsByClassName("result")[0];
 
+Array.from(buttons).forEach(button => {
+    button.addEventListener("click", e => {
+        var buttonText = button.innerText;
+        if (buttonText === "C") {
+            textbox.value = " ";
+            result.innerText=" ";
+        }
+        else if(buttonText==="D")
+            textbox.value=textbox.value.slice(0,-1);
+        else if (buttonText === "=") 
+        {
+            textbox.value=result.innerText;
+            result.innerText=" ";
+        }
+        else 
+            textbox.value += buttonText;
+           try{
+            var numbers=textbox.value.match(/\d+(\.\d+)?/g).reverse();
+            var operation=textbox.value.match(/[\+\-\x\%\÷]/g).reverse();
+            while(operation!=0){
+                var symbol=operation.pop();
+                if(symbol=="+"){
+                     num1=parseFloat(numbers.pop());
+                     num2=parseFloat(numbers.pop());
+                     numbers.push(num1+num2);
+                     result.innerText=numbers;
+                }
+                else if(symbol== "-"){
+                     num1=parseFloat(numbers.pop());
+                     num2=parseFloat(numbers.pop());
+                     numbers.push(num1-num2);
+                     result.innerText=numbers;
+                }
+                else if(symbol== "x"){
+                     num1=parseFloat(numbers.pop());
+                     num2=parseFloat(numbers.pop());
+                     numbers.push(num1*num2);
+                     result.innerText=numbers;
+                }
+                else if(symbol== "÷"){
+                     num1=parseFloat(numbers.pop());
+                     num2=parseFloat(numbers.pop());
+                     numbers.push(num1/num2);
+                     result.innerText=numbers;
+                }
+                else if(symbol== "%"){
+                     num1=parseFloat(numbers.pop());
+                     num2=parseFloat(numbers.pop());
+                     numbers.push((num1*num2)/100);
+                     result.innerText=numbers;
+                 }
+             }
+           }
+           catch(error){
+               result.innerText="";                
+           }         
+    });
+});
