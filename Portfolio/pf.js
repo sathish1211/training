95c828231bb12739ae43d3bec9080fd7867902d3
sathish1211 <sathish2001p@gmail.com>
2023-10-11 19:02:40 +0530

    mySelf

---
+++ Portfolio/pf.js
@@ -0,0 +1,6 @@
+// const menuIcon = document.getElementsByClassName('menu');
+// const menu = document.getElementsByTagName('ul')[0];
+
+// menuIcon.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+// });
